058007ee50306e163c7c6dcf6e82eb6aef71e4c0
bahauddin fahad <bahauddin.fahad819@gmail.com>
2024-11-11 21:12:35 +0600

    modules added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,30 @@
 import express from "express";
+import { BookRoutes } from "../modules/Book/book.route";
+import { BorrowBookRoutes } from "../modules/BorrowBook/borrowBook.route";
+import { MemberRoutes } from "../modules/Member/member.route";
+import { ReturnBookRoutes } from "../modules/BorrowBook/return.route";
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    // path: "/user",
-    // route: UserRoutes,
+    path: "/books",
+    route: BookRoutes,
+  },
+  {
+    path: "/members",
+    route: MemberRoutes,
+  },
+  {
+    path: "/borrow",
+    route: BorrowBookRoutes,
+  },
+  {
+    path: "/return",
+    route: ReturnBookRoutes,
   },
 ];
 
-// moduleRoutes.forEach((route) => router.use(route.path, route.route));
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
